88a738c4084a09bc1b5e9aa73dc9986cbb8e43ec
spt3g <spt3g@spt3g.fnal.gov>
2015-10-27 00:42:49 -0500

    Total rewrite of fridge_logger. Much more compact and easy to reconfigure the lots. Hopefully more reliable too.

--- logger/fridge_logger.py
+++ logger/fridge_logger.py
@@ -14,52 +14,44 @@ import socket
 import time
 import numpy as np
 import tables
-import lakeshore218
-import lakeshore350
-import shutil
 import os.path
 
 import plotter
 
-# channel labels
-labels_lakeshore_218_1 = {'record time': tables.Time32Col(pos=0),
-                          'HEX': tables.Float32Col(pos=1),
-                          'mainplate': tables.Float32Col(pos=2),
-                          'He4 IC Pump': tables.Float32Col(pos=3),
-                          'He3 IC Pump': tables.Float32Col(pos=4),
-                          'He3 UC Pump': tables.Float32Col(pos=5),
-                          'He4 IC Switch': tables.Float32Col(pos=6),
-                          'He3 IC Switch': tables.Float32Col(pos=7),
-                          'He3 UC Switch': tables.Float32Col(pos=8),
-                          }
-keys_lakeshore_218_1 = ['record time', 'HEX', 'mainplate', 'He4 IC Pump', 'He3 IC Pump',
-                        'He3 UC Pump', 'He4 IC Switch', 'He3 IC Switch',  'He3 UC Switch']
-labels_lakeshore_218_2 = {'record time': tables.Time32Col(pos=0),
-                          'PTC 4K stage': tables.Float32Col(pos=1),
-                          'PTC 50K stage': tables.Float32Col(pos=2),
-                          'channel 2': tables.Float32Col(pos=3),
-                          'channel 3': tables.Float32Col(pos=4),
-                          'wiring harness': tables.Float32Col(pos=5),
-                          '4K shield near harness': tables.Float32Col(pos=6),
-                          '3G SQUIDs': tables.Float32Col(pos=7),
-                          'SZ SQUIDs': tables.Float32Col(pos=8),
-                          }
-keys_lakeshore_218_2 = ['record time', 'PTC 4K stage', 'PTC 50K stage', 'channel 2', 'channel 3',
-                        'wiring harness', '4K shield near harness', '3G SQUIDs',  'SZ SQUIDs']
-labels_lakeshore_350_1 = {'record time': tables.Time32Col(pos=0),
-                          'UC Head': tables.Float32Col(pos=1),
-                          'IC Head': tables.Float32Col(pos=2),
-                          'LC shield': tables.Float32Col(pos=3),
-                          'LC board': tables.Float32Col(pos=4),
-                          }
-keys_lakeshore_350_1 = ['record time', 'UC Head', 'IC Head', 'LC shield', 'LC board']
-labels_lakeshore_350_2 = {'record time': tables.Time32Col(pos=0),
-                          'backplate': tables.Float32Col(pos=1),
-                          'channel B': tables.Float32Col(pos=2),
-                          'channel C': tables.Float32Col(pos=3),
-                          'channel D': tables.Float32Col(pos=4),
-                        }
-keys_lakeshore_350_2 = ['record time', 'backplate', 'channel B', 'channel C', 'channel D']
+
+# BASIC CONFIGURATION
+# Specify mapping of {"interface" -> [list of channels]} here. The "interface"
+# is simply the serial interface or IP address of the Lakeshore box.
+channel_map = {'/dev/ttyr00':   ['HEX', 'mainplate', 'He4 IC Pump', 'He3 IC Pump',
+                                 'He3 UC Pump', 'He4 IC Switch', 'He3 IC Switch',  'He3 UC Switch'],
+               '/dev/ttyr01':   ['PTC 4K stage', 'PTC 50K stage', 'channel 2', 'channel 3',
+                                 'wiring harness', '4K shield near harness', '3G SQUIDs',  'SZ SQUIDs'],
+               '192.168.0.12':  ['UC Head', 'IC Head', 'LC shield', 'LC board'],
+               '192.168.2.5':   ['backplate', 'channel B', 'channel C', 'channel D']}
+
+# Specify the variables to be plotted in each subplot of the display. This
+# should be a list of lists of strings, where each list contains the variables
+# contained in one subplot, and each string should be one of the variable names
+# in the 'channel_map' above
+plot_list = [[['He4 IC Pump', 'He3 IC Pump', 'He3 UC Pump'], ['He4 IC Switch', 'He3 IC Switch', 'He3 UC Switch']],
+             [['HEX', 'mainplate']],
+             [['PTC 4K stage'], ['PTC 50K stage']],
+             [['UC Head', 'IC Head', 'LC shield', 'LC board']],
+             [['wiring harness', '4K shield near harness', '3G SQUIDs', 'SZ SQUIDs']]]
+
+# Specify mapping of {"interface" -> "human-readable description"}. This isn't
+# used for anything specific right now, but it is worth having some more human-
+# readable description about what interface name corresponds to which box, just
+# in case it is ever needed somewhere, or minimally just for code readability.
+interface_map = {}
+
+
+# convenience functions for adding and removing underscores
+def underscoreify(string):
+    return string.replace(' ', '_')
+def deunderscoreify(string):
+    return string.replace('_', ' ')
+
 
 # update frequency
 dt_update = 10  # sec
@@ -70,77 +62,81 @@ if os.path.isfile(data_filename) == True:
     print data_filename + ' already exists. Attempting to append data to end of file. If thermometer names differ in the existing file, this may fail.'
     pytables_mode = 'a'
 else:
-    print 'Attempting to create data file ' + data_filename '.'
+    print 'Attempting to create data file ' + data_filename
     pytables_mode = 'w'
 
 # create output file
 f_h5 = tables.open_file(data_filename, mode=pytables_mode, title='fridge data') # append file, by default
 try:
-    # try pulling the tables from the file, which should work if it exists
     group_all_data = f_h5.get_node('/data')
-    table_lakeshore_218_1 = f_h5.get_node('/data/LS_218_1')
-    table_lakeshore_218_2 = f_h5.get_node('/data/LS_218_2')
-    table_lakeshore_350_1 = f_h5.get_node('/data/LS_350_1')
-    table_lakeshore_350_2 = f_h5.get_node('/data/LS_350_2')
 except tables.NoSuchNodeError:
-    # otherwise, the file presumably doesn't exist and we need new tables
     group_all_data = f_h5.create_group('/', 'data', 'all data')
-    table_lakeshore_218_1 = f_h5.create_table(group_all_data, 'LS_218_1', labels_lakeshore_218_1, "Data from Lakeshore 218 #1")
-    table_lakeshore_218_2 = f_h5.create_table(group_all_data, 'LS_218_2', labels_lakeshore_218_2, "Data from Lakeshore 218 #2")
-    table_lakeshore_350_1 = f_h5.create_table(group_all_data, 'LS_350_1', labels_lakeshore_350_1, "Data from Lakeshore 350 #1")
-    table_lakeshore_350_2 = f_h5.create_table(group_all_data, 'LS_350_2', labels_lakeshore_350_2, "Data from Lakeshore 350 #2")
+tables_list = dict()
+for interface in channel_map:
+    for channel in channel_map[interface]:
+        try:
+            # try pulling table from the file, which should work if it exists
+            tables_list[channel] = f_h5.get_node('/data/' + underscoreify(channel))
+        except tables.NoSuchNodeError:
+            # otherwise, the file presumably doesn't exist and we need new tables
+            table_columns = {'time': tables.Time32Col(), channel: tables.Float32Col()}
+            tables_list[channel] = f_h5.create_table(group_all_data, underscoreify(channel), table_columns, channel)
 
-# set up serial ports
-lakeshore_218_1 = serial.Serial('/dev/ttyr00', 9600, serial.SEVENBITS, serial.PARITY_ODD, serial.STOPBITS_ONE )
-lakeshore_218_2 = serial.Serial('/dev/ttyr01', 9600, serial.SEVENBITS, serial.PARITY_ODD, serial.STOPBITS_ONE )
+# split up interface addresses by serial and TCP
+serial_interface_address = [name for name in channel_map.keys() if '/dev' in name]
+tcp_interface_address = [name for name in channel_map.keys() if '192.168' in name]
+# check that we didn't miss interfaces in the split
+if set(serial_interface_address + tcp_interface_address) != set(channel_map.keys()):
+    print "WARNING: Could not parse all interface addresses as either serial or TCP. " + \
+          "Some devices may not be read!"
 
-# set up ethernet for Lakeshore 350
-# please note that ethernet settings different from these WILL NOT WORK!
-# in addition, the '\r\n' in the message sent to the lakeshore is MANDATORY!
-address_lakeshore_350_1 = ('192.168.0.12', 7777) # default settings for the lakeshore 350 #1
-lakeshore_350_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lakeshore_350_1.connect(address_lakeshore_350_1)
-lakeshore_350_1.settimeout(1.0)
-address_lakeshore_350_2 = ('192.168.2.5', 7777)  # default settings for the lakeshore 350 #2 (when #1 is plugged in!)
-lakeshore_350_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lakeshore_350_2.connect(address_lakeshore_350_2)
-lakeshore_350_2.settimeout(1.0)
+# set up the serial interfaces
+serial_interfaces = dict()
+for interface_address in serial_interface_address:
+    serial_interfaces[interface_address] = serial.Serial(interface_address, 9600, serial.SEVENBITS, serial.PARITY_ODD, serial.STOPBITS_ONE)
+
+# setup the TCP interfaces
+tcp_interfaces = dict()
+for interface_address in tcp_interface_address:
+    tcp_interfaces[interface_address] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_interfaces[interface_address].connect((interface_address, 7777))
+    tcp_interfaces[interface_address].settimeout(1.0)
 
 
-# main loop
+# main data acquisition loop
 try:
     while True:
-        # query the Lakeshore devices
-        lakeshore_218_1.write('KRDG?\r\n')
-        lakeshore_218_2.write('KRDG?\r\n')
-        lakeshore_350_1.sendto('KRDG? 0\r\n', address_lakeshore_350_1)
-        lakeshore_350_2.sendto('KRDG? 0\r\n', address_lakeshore_350_2)
+        # query the devices
+        for interface_name in serial_interfaces:
+            serial_interfaces[interface_name].write('KRDG?\r\n')
+        for interface_address in tcp_interfaces:
+            tcp_interfaces[interface_address].sendto('KRDG? 0\r\n', (interface_address, 7777))
+        current_time = time.time()
 
-        # wait for the Lakeshores to issue a response
+        # wait for devices to issue a response
         time.sleep(0.2)
 
-        # read the response
-        out_lakeshore_218_1 = lakeshore_218_1.read(lakeshore_218_1.inWaiting())
-        out_lakeshore_218_2 = lakeshore_218_2.read(lakeshore_218_2.inWaiting())
-        out_lakeshore_350_1, _ = lakeshore_350_1.recvfrom(2048)
-        out_lakeshore_350_2, _ = lakeshore_350_2.recvfrom(2048)
+        # read the responses
+        for interface_name in serial_interfaces:
+            raw_output = serial_interfaces[interface_name].read(serial_interfaces[interface_name].inWaiting())
+            for jValue in range(len(channel_map[interface_name])):
+                channel_name = channel_map[interface_name][jValue]
+                tables_list[channel_name].row['time'] = current_time
+                tables_list[channel_name].row[channel_name] = float(raw_output.split(',')[jValue])
+                tables_list[channel_name].row.append()
+                tables_list[channel_name].flush()
 
-        # write the data to a pytables file
-        lakeshore218.write(table_lakeshore_218_1.row, keys_lakeshore_218_1, out_lakeshore_218_1)
-        table_lakeshore_218_1.flush()
-        lakeshore218.write(table_lakeshore_218_2.row, keys_lakeshore_218_2, out_lakeshore_218_2)
-        table_lakeshore_218_2.flush()
-        lakeshore350.write(table_lakeshore_350_1.row, keys_lakeshore_350_1, out_lakeshore_350_1)
-        table_lakeshore_350_1.flush()
-        lakeshore350.write(table_lakeshore_350_2.row, keys_lakeshore_350_2, out_lakeshore_350_2)
-        table_lakeshore_350_2.flush()
+        for interface_name in tcp_interfaces:
+            raw_output, _ = tcp_interfaces[interface_name].recvfrom(2048)
+            for jValue in range(len(channel_map[interface_name])):
+                channel_name = channel_map[interface_name][jValue]
+                tables_list[channel_name].row['time'] = current_time
+                tables_list[channel_name].row[channel_name] = float(raw_output.split(',')[jValue])
+                tables_list[channel_name].row.append()
+                tables_list[channel_name].flush()
 
-        # duplicate the pytables file so that it can be read by other processes
-        # as needed
-        shutil.copyfile(data_filename, data_filename + str('.lock'))
-        shutil.move(data_filename + str('.lock'), data_filename.strip('.h5') + '_read.h5')
-
-        plotter.update_plot(table_lakeshore_218_1, table_lakeshore_218_2, table_lakeshore_350_1, table_lakeshore_350_2)
+        # update the plots
+        plotter.update_plot(tables_list, plot_list)
 
         # wait before reading again
         time.sleep(dt_update)
@@ -149,7 +145,7 @@ except KeyboardInterrupt:
     print "\nStopping data acquisition"
 
 # close the connections
-lakeshore_218_1.close()
-lakeshore_218_2.close()
-lakeshore_350_1.close()
-lakeshore_350_2.close()
+for interface_address in serial_interface_address:
+    serial_interfaces[interface_address].close()
+for interface_address in tcp_interface_address:
+    tcp_interfaces[interface_address].close()
--- logger/plotter.py
+++ logger/plotter.py
@@ -14,118 +14,47 @@ import matplotlib.dates as mdates
 import datetime
 import numpy as np
 
-def update_plot(table_LS_218_1, table_LS_218_2, table_LS_350_1, table_LS_350_2):
-    time_to_show = 3600 # [s]
-
-    # extract data from table
-    maxtime = [row['record time'] for row in table_LS_218_1.iterrows(start=table_LS_218_1.nrows-1, stop=table_LS_218_1.nrows)]
-    time_1 = np.array([x['record time'] for x in table_LS_218_1.iterrows() if x['record time']>(maxtime[0]-time_to_show)])
-    plottime_1 = [datetime.datetime.fromtimestamp(ts) for ts in time_1]
-    data_LS_218_1 = dict()
-    for col in table_LS_218_1.colnames:
-        coldata = np.array([x[col] for x in table_LS_218_1.iterrows() if x['record time']>(maxtime[0]-time_to_show)])
-        data_LS_218_1[col] = coldata
-
-    time_2 = np.array([x['record time'] for x in table_LS_218_2.iterrows() if x['record time']>(maxtime[0]-time_to_show)])
-    plottime_2 = [datetime.datetime.fromtimestamp(ts) for ts in time_2]
-    data_LS_218_2 = dict()
-    for col in table_LS_218_2.colnames:
-        coldata = np.array([x[col] for x in table_LS_218_2.iterrows() if x['record time']>(maxtime[0]-time_to_show)])
-        data_LS_218_2[col] = coldata
-
-    time_350_1 = np.array([x['record time'] for x in table_LS_350_1.iterrows() if x['record time']>(maxtime[0]-time_to_show)])
-    plottime_350_1 = [datetime.datetime.fromtimestamp(ts) for ts in time_350_1]
-    data_LS_350_1 = dict()
-    for col in table_LS_350_1.colnames:
-        coldata = np.array([x[col] for x in table_LS_350_1.iterrows() if x['record time']>(maxtime[0]-time_to_show)])
-        data_LS_350_1[col] = coldata
-
-    time_350_2 = np.array([x['record time'] for x in table_LS_350_2.iterrows() if x['record time']>(maxtime[0]-time_to_show)])
-    plottime_350_2 = [datetime.datetime.fromtimestamp(ts) for ts in time_350_2]
-    data_LS_350_2 = dict()
-    for col in table_LS_350_2.colnames:
-        coldata = np.array([x[col] for x in table_LS_350_2.iterrows() if x['record time']>(maxtime[0]-time_to_show)])
-        data_LS_350_2[col] = coldata
-
-
-    # do the plotting
-    #  Set labelsizes
-    mpl.rc('xtick', labelsize=8)
-    mpl.rc('ytick', labelsize=8)
-
+def update_plot(tables_list, plot_list):
+    time_to_show = 3600 #[s]
+    num_subplots = len(plot_list)
 
     f = plt.figure(1,figsize=(7,10))
-    ax1 = plt.subplot(511)
-    ax1_twin = ax1.twinx()
-    lns = []
-    for col in table_LS_218_1.colnames:
-        if 'Pump' in col:
-            plt.sca(ax1)
-            lns = lns + plt.plot(plottime_1, data_LS_218_1[col], label=col)
-        if 'Switch' in col:
-            plt.sca(ax1_twin)
-            lns = lns + plt.plot(plottime_1, data_LS_218_1[col], label=col, linestyle='--')
-    plt.sca(ax1)
-    plt.ylabel('Pump Temp. [K]',fontsize=10)
-    plt.xlabel('Time')
-    plt.sca(ax1_twin)
-    plt.ylabel('Switch Temp. [K]',fontsize=10)
-    labs = [l.get_label() for l in lns]
-    plt.legend(lns, labs, loc="upper left", prop={'size':7}, bbox_to_anchor=[1.1,1.0], borderpad=.2)
-    plt.autoscale()
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 
+    for jplot in range(len(plot_list)):
+        # make one subplot per entry in plot_list
+        ax = plt.subplot(num_subplots*100 + 10 + jplot+1)
+        
+        l1 = []
+        l2 = []
+        for var in plot_list[jplot][0]:
+            maxtime = [row['time'] for row in tables_list[var].iterrows(start=tables_list[var].nrows-1, stop=tables_list[var].nrows)]
+            time_1 = np.array([x['time'] for x in tables_list[var].iterrows() if x['time']>(maxtime[0]-time_to_show)])
+            plottime_1 = [datetime.datetime.fromtimestamp(ts) for ts in time_1]
+            coldata = np.array([x[var] for x in tables_list[var].iterrows() if x['time']>(maxtime[0]-time_to_show)])
+            l1 = plt.plot(plottime_1, coldata, label=var)
+            plt.ylabel('Temperature [K]',fontsize=10)
+        # add another axis if indicated by plot_list argument
+        if len(plot_list[jplot]) == 2:
+            ax_twin = ax.twinx()
+            for var in plot_list[jplot][1]:
+                maxtime = [row['time'] for row in tables_list[var].iterrows(start=tables_list[var].nrows-1, stop=tables_list[var].nrows)]
+                time_1 = np.array([x['time'] for x in tables_list[var].iterrows() if x['time']>(maxtime[0]-time_to_show)])
+                plottime_1 = [datetime.datetime.fromtimestamp(ts) for ts in time_1]
+                coldata = np.array([x[var] for x in tables_list[var].iterrows() if x['time']>(maxtime[0]-time_to_show)])
+                l2 = plt.plot(plottime_1, coldata, label=var, linestyle='--')
+        
+                lns = l1+l2
+                labs = [l.get_label() for l in lns]
+                ax.legend(lns, labs, loc='upper left', prop={'size':7}, bbox_to_anchor=[1.1,1.0], borderpad=.2)
+        else:
+            plt.legend(loc='upper left', prop={'size':7}, bbox_to_anchor=[1.0,1.0], borderpad=.2)
 
-    ax2 = plt.subplot(512)
-    plt.ylabel('Temperature [K]',fontsize=10)
-    plt.plot(plottime_1, data_LS_218_1['HEX'], label='HEX')
-    plt.plot(plottime_1, data_LS_218_1['mainplate'], label='mainplate')
-    plt.legend(loc='upper left', prop={'size':7}, bbox_to_anchor=[1.0,1.0], borderpad=.2)
-    plt.autoscale()
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+        plt.autoscale()
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+        if jplot+1 != len(plot_list):
+            ax.get_xaxis().set_ticks([])
 
-
-    ax3 = plt.subplot(513)
-    l1 = plt.plot(plottime_2, data_LS_218_2['PTC 4K stage'], label='PTC 4K stage')
-    plt.autoscale()
-    plt.ylabel('4K Stage Temp. [K]',fontsize=10)
-    ax3_1 = ax3.twinx()
-    l2 = plt.plot(plottime_2, data_LS_218_2['PTC 50K stage'], label='PTC 50K stage', color='r')
-    plt.ylabel('50K Stage Temp. [K]',fontsize=10)
-    # for tl in ax3_1.get_yticklabels():
-    #     tl.set_color('r')
-    lns = l1 + l2
-    labs = [l.get_label() for l in lns]
-    ax3.legend(lns, labs, loc='upper left', prop={'size':7}, bbox_to_anchor=[1.1,1.0], borderpad=.2)
-    plt.autoscale()
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-
-
-    ax4 = plt.subplot(514)
-    for col in table_LS_350_1.colnames:
-        if col != 'record time':
-            plt.plot(plottime_350_1, data_LS_350_1[col], label=col)
-        plt.plot(plottimes_350_2, data_LS_350_2['backplate'], label='backplate')
-    plt.ylabel('Temperature [K]',fontsize=10)
-    plt.xlabel('Time')
-    plt.legend(loc="upper left", prop={'size':7}, bbox_to_anchor=[1.0,1.0], borderpad=.2)
-    plt.autoscale()
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-
-
-    ax5 = plt.subplot(515)
-    plt.plot(plottime_350_1, data_LS_218_2['wiring harness'], label='wiring harness')
-    plt.plot(plottime_350_1, data_LS_218_2['4K shield near harness'], label='4K shield near harness')
-    plt.plot(plottime_350_1, data_LS_218_2['3G SQUIDs'], label='3G SQUIDs')
-    plt.plot(plottime_350_1, data_LS_218_2['SZ SQUIDs'], label='SZ SQUIDs')
-    plt.ylabel('Temperature [K]',fontsize=10)
-    plt.xlabel('Time')
-    plt.legend(loc="upper left", prop={'size':7}, bbox_to_anchor=[1.0,1.0], borderpad=.2)
-    plt.autoscale()
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-
-    #  Adjust the size of the plot areas
+    plt.xlabel('time')
     plt.subplots_adjust(left=0.10, bottom=0.1, right=0.75, top=0.92, wspace=0.21, hspace=0.1)
-
-    plt.savefig('plotter/figures/temperature_plot.png')
+    plt.savefig('figures/temperature_plot.png')
     plt.clf()
