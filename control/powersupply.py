c0687698a6c83a96a6bf1de3df55af62d92b5e3b
Adam Anderson <adama@fnal.gov>
2016-03-10 17:26:53 -0600

    First commit of power supply management code.

---
+++ control/powersupply.py
@@ -0,0 +1,9 @@
+# powersupply.py
+#
+# Python module for managing I/O with power supplies. This module must be edited
+# by the user for each site application because different models of power supply
+# have different commands that are used to get/set voltages.
+#
+# Adam Anderson
+# adama@fnal.gov
+# 10 March 2016
