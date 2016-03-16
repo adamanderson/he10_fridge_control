36294d6b8a2a43e5b11da41de21608a73b06e89a
Adam Anderson <adama@fnal.gov>
2016-03-16 18:50:02 -0500

    Update some comment code in powersupply.py and remove deprecated module voltages.py

--- control/powersupply.py
+++ control/powersupply.py
@@ -4,6 +4,18 @@
 # by the user for each site application because different models of power supply
 # have different commands that are used to get/set voltages.
 #
+# In the first few lines of the script, the following things should occur:
+# 1.) Serial interfaces should be defined for all power supplies (e.g. a
+# serial.Serial object).
+# 2.) Serial interfaces must be mapped to each pump and heater in the dictionary
+# 'serial_interface'.
+# 3.) The commands to send to read each voltage must be specified in the 
+# dictionary 'read_cmd'.
+# 4.) The commands to sedn to write each voltage must be specified in the
+# dictionary 'write_cmd'.
+# 5.) The termination characters to add to the end of each read/write command
+# (typically '\r\n') should be specified in the variable termination_str.
+#
 # Adam Anderson
 # adama@fnal.gov
 # 10 March 2016
@@ -13,6 +25,7 @@ import serial
 heaternames = ['3He UC pump', '3He IC pump', '4He IC pump',
                '3He UC switch', '3He IC switch', '4He IC switch']
 
+# serial interfaces to power supplies
 ttyr02 = serial.Serial(port='/dev/ttyr02',
                        baudrate=9600,
                        parity=serial.PARITY_ODD,
@@ -24,26 +37,31 @@ ttyr03 = serial.Serial(port='/dev/ttyr03',
                        stopbits=serial.STOPBITS_ONE,
                        bytesize=serial.SEVENBITS)
 
+# mapping of serial interfaces to heaters
 serial_interface = {'3He UC pump': ttyr02,
                     '3He IC pump': ttyr03,
                     '4He IC pump': ttyr03,
                     '3He UC switch': ttyr02,
                     '3He IC switch': ttyr02,
                     '4He IC switch': ttyr03}
-# do not include termination string in the following commands (typically '\r\n')
-# for Keysight/Agilent devices
+
+# commands to read from power supplies
 read_cmd =  {'3He UC pump': 'MEAS:VOLT:DC? N25V',
              '3He IC pump': 'MEAS:VOLT:DC? P25V',
              '4He IC pump': 'MEAS:VOLT:DC? N25V',
              '3He UC switch': 'MEAS:VOLT:DC? P6V',
              '3He IC switch': 'MEAS:VOLT:DC? P25V',
              '4He IC switch': 'MEAS:VOLT:DC? P6V'}
+
+# commands to write to power supplies
 write_cmd = {'3He UC pump': 'APPL N25V,',
              '3He IC pump': 'APPL P25V,',
              '4He IC pump': 'APPL N25V,',
              '3He UC switch': 'APPL P6V,',
              '3He IC switch': 'APPL P25V,',
              '4He IC switch': 'APPL P6V,'}
+
+# termination character to add onto commands to send
 termination_str = '\r\n'
 
 
