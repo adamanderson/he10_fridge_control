8c37b86f255f38a78a19888875cbaafd3d3b7da3
Faustin Carter <faustin.carter@gmail.com>
2017-02-04 15:40:33 -0600

    rewritten to be more fault tolerant, hopefully

--- logger/fridge_logger_anl.py
+++ logger/fridge_logger_anl.py
@@ -1,22 +1,25 @@
 # fridge_logger.py
 #
-# A logging script for the He10 fridge that records temperatures. The data is
+# A logging script for the ANL fridge that records temperatures. The data is
 # saved in a text file (or perhaps hdf5 file in the future) and can be accessed
 # later by the plotting script or the fridge control script when it performs
 # an automatic fridge cycle.
 #
+# Original code by:
 # Adam Anderson
 # adama@fnal.gov
 # 12 September 2015
+#
+# Modified heavily by:
+# Faustin Carter
+# Feb. 4, 2016
 
 import serial
-import socket
 import time
 import numpy as np
 import tables
 import os.path
 import shutil
-import collections as clcs
 
 import plotter
 
@@ -24,10 +27,22 @@ import plotter
 # BASIC CONFIGURATION
 # Specify mapping of {"interface" -> [list of channels]} here. The "interface"
 # is simply the serial interface or IP address of the Lakeshore box.
-channel_map = {'/dev/ttyr13':   ['He4 IC Switch', 'He3 IC Switch', 'He3 UC Pump', 'He3 UC Switch',
-                                    'He3 IC Pump', 'mainplate', 'HEX', 'He4 IC Pump'],
-               '/dev/ttyr18':   ['UC Head', 'IC Head', 'PTC 50K Plate', 'PTC 4K Plate',
-                                 'SQUID D1', 'SQUID D2']}
+# List of channels contains tuple of (channel name, channel number)
+channel_map = {'/dev/ttyr13':   [('He4 IC Switch',  '1'),
+                                 ('He3 IC Switch',  '2'),
+                                 ('He3 UC Pump',    '3'),
+                                 ('He3 UC Switch',  '4'),
+                                 ('He3 IC Pump',    '5'),
+                                 ('mainplate',      '6'),
+                                 ('HEX',            '7'),
+                                 ('He4 IC Pump',    '8')],
+
+               '/dev/ttyr18':   [('UC Head',        'A'),
+                                 ('IC Head',        'B'),
+                                 ('PTC 50K Plate',  'C2'),
+                                 ('PTC 4K Plate',   'C1'),
+                                 ('SQUID D1',       'D1'),
+                                 ('SQUID D2',       'D2')]}
 
 # Specify the variables to be plotted in each subplot of the display. This
 # should be a list of lists of (1 or 2) lists of strings, where each outer list contains the variables
@@ -35,30 +50,15 @@ channel_map = {'/dev/ttyr13':   ['He4 IC Switch', 'He3 IC Switch', 'He3 UC Pump'
 # one or two y-axis scales should be used, and the variable names in the
 # final lists are the names from 'channel_map' above which should be mapped to
 # each y-axis scale.
-plot_list = [[['He4 IC Pump', 'He3 IC Pump', 'He3 UC Pump'], ['He4 IC Switch', 'He3 IC Switch', 'He3 UC Switch']],
+plot_list = [[['He4 IC Pump', 'He3 IC Pump', 'He3 UC Pump'],
+                ['He4 IC Switch', 'He3 IC Switch', 'He3 UC Switch']],
              [['HEX', 'mainplate']],
              [['PTC 4K Plate'], ['PTC 50K Plate']],
              [['UC Head', 'IC Head']],
              [['SQUID D1', 'SQUID D2']]]
 
-# Specify mapping of {"interface" -> "human-readable description"}. This isn't
-# used for anything specific right now, but it is worth having some more human-
-# readable description about what interface name corresponds to which box, just
-# in case it is ever needed somewhere, or minimally just for code readability.
-interface_map = {'/dev/ttyr13':'LS218',
-                '/dev/ttyr18':'LS340'}
-
-split_char_map = {'LS340':';',
-                    'LS218':','}
-
-#Coopting this to use as LS340 Channel mapper since you can't just grab all channels at once.
-channel_maps = {'LS340': clcs.OrderedDict({'A':'UC Head',
-                'B': 'IC Head',
-                'C1': 'PTC 4K Plate',
-                'C2': 'PTC 50K Plate',
-                'D1': 'SQUID D1',
-                'D2': 'SQUID D2'}),
-                'LS218': None}
+# update frequency
+dt_update = 2  # sec
 
 
 # convenience functions for adding and removing underscores
@@ -67,9 +67,43 @@ def underscoreify(string):
 def deunderscoreify(string):
     return string.replace('_', ' ')
 
+#A handy function to read one temperature point from one channel.
+#This is less efficient than reading all channels at once, but much easier
+#to recover from when something goes wrong.
+def read_temp_LS(interface, channel, num_trys = 10, delay = 0.1):
+    """First attempt at making a reader that does a little bit of data validation"""
+    for num in range(num_trys):
+        #flush the I/O buffers
+        interface.flushInput()
+        interface.flushOutput()
 
-# update frequency
-dt_update = 2  # sec changed to 1 sec for RT
+        #Ask for the temperature
+        interface.write('KRDG? ' + channel + '\r\n')
+
+        #Wait a hot tenth of a second
+        time.sleep(delay)
+
+        #Read all the bytes that are at the port
+        raw_output += interface.read(interface.inWaiting())
+
+        #Check that the string ends with the terminator. If not, try again
+        if raw_output[-2:] == '\r\n'
+            #If the data is bad or incomplete, float conversion may fail
+            try:
+                #Strip off the term char (usually '\r\n'))
+                #and convert to float
+                temp = float(raw_output.strip())
+            except ValueError:
+                pass
+            else:
+                return temp
+
+        #Welp, that didn't work, try again!
+        print 'Problem reading from: ' + interface + ':' + channel
+        print 'Will try ' + str(num_trys - 1 - num) + ' more times before aborting.'
+        time.sleep(delay)
+
+    raise NameError("Lost communication with: " + interface)
 
 # file name
 data_filename = raw_input('Enter relative path to data file (must end in .h5). NB: If enter an existing filename, the script will attempt to append that file, by default: ')
@@ -97,104 +131,31 @@ for interface in channel_map:
             table_columns = {'time': tables.Time32Col(), channel: tables.Float32Col()}
             tables_list[channel] = f_h5.create_table(group_all_data, underscoreify(channel), table_columns, channel)
 
-# split up interface addresses by serial and TCP
-serial_interface_address = [name for name in channel_map.keys() if '/dev' in name]
-tcp_interface_address = [name for name in channel_map.keys() if '192.168' in name]
-# check that we didn't miss interfaces in the split
-if set(serial_interface_address + tcp_interface_address) != set(channel_map.keys()):
-    print "WARNING: Could not parse all interface addresses as either serial or TCP. " + \
-          "Some devices may not be read!"
-
 # set up the serial interfaces
 serial_interfaces = dict()
-for interface_address in serial_interface_address:
-    serial_interfaces[interface_address] = serial.Serial(interface_address, 9600, serial.SEVENBITS, serial.PARITY_ODD, serial.STOPBITS_ONE)
-
-# setup the TCP interfaces
-tcp_interfaces = dict()
-for interface_address in tcp_interface_address:
-    tcp_interfaces[interface_address] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_interfaces[interface_address].connect((interface_address, 7777))
-    tcp_interfaces[interface_address].settimeout(1.0)
+for interface_address in channel_map.keys():
+    serial_interfaces[interface_address] = serial.Serial(interface_address,
+                                                         9600,
+                                                         serial.SEVENBITS,
+                                                         serial.PARITY_ODD,
+                                                         serial.STOPBITS_ONE)
 
 
 # main data acquisition loop
 try:
     while True:
-        firstRun = False
-        # query the devices
-        for interface_name in serial_interfaces:
-            if interface_map[interface_name] is 'LS218':
-                serial_interfaces[interface_name].write('KRDG?\r\n')
-            elif interface_map[interface_name] is 'LS340':
-                tmp_query = ""
-                for ch_num, ch_name in channel_maps[interface_map[interface_name]].iteritems():
-                    tmp_query += 'KRDG? ' + ch_num + ';'
 
-                tmp_query = tmp_query[:-1] + '\r\n'
-                if firstRun:
-                    print tmp_query
+        for serial_interface in serial_interfaces:
+            for channel_name, channel_num in channel_map[serial_interface]:
+                #Fill up the columns of a table row with data
+                tables_list[channel_name].row[channel_name] = read_temp_LS(serial_interface, channel_num)
+                tables_list[channel_name].row['time'] = time.time()
 
-                serial_interfaces[interface_name].write(tmp_query)
-            else:
-                raise NameError("Unknown device.")
-
-        for interface_address in tcp_interfaces:
-            tcp_interfaces[interface_address].sendto('KRDG? 0\r\n', (interface_address, 7777))
-        current_time = time.time()
-
-        # wait for devices to issue a response
-        time.sleep(0.1)
-
-        # read the responses for serial interfaces
-        for interface_name in serial_interfaces:
-            raw_output = serial_interfaces[interface_name].read(serial_interfaces[interface_name].inWaiting())
-
-            # check that we actually got a response with data in it (occasionally the MOXA
-            # is non-responsive), otherwise do nothing
-            split_char = split_char_map[interface_map[interface_name]]
-
-            if firstRun:
-                print interface_name + '\n'
-                print raw_output + '\n'
-                print raw_output.split(split_char)
-                print '\n'
-
-            if len(raw_output.split(split_char)) > 0:
-                for jValue in range(len(channel_map[interface_name])):
-                    channel_name = channel_map[interface_name][jValue]
-                    tables_list[channel_name].row['time'] = current_time
-                    try:
-                        tables_list[channel_name].row[channel_name] = float(raw_output.split(split_char)[jValue])
-                    except ValueError:
-                        print repr(raw_output.split(split_char)[jValue])
-                        tables_list[channel_name].row[channel_name] = float(raw_output.strip().split(split_char)[jValue])
-                    #tables_list[channel_name].row[channel_name] = float(raw_output.split(split_char)[jValue])
-                    tables_list[channel_name].row.append()
-                    tables_list[channel_name].flush()
-
-        # for the TCP interfaces, also get the raw sensor values and substitute these
-        # in for the temperature if the temperature reads exactly zero
-        raw_output_temp = dict()
-        raw_output_resist = dict()
-        for interface_address in tcp_interfaces:
-            raw_output_temp[interface_address], _ = tcp_interfaces[interface_address].recvfrom(2048)
-        for interface_address in tcp_interfaces:
-            tcp_interfaces[interface_address].sendto('SRDG? 0\r\n', (interface_address, 7777))
-        time.sleep(0.1)
-        for interface_address in tcp_interfaces:
-            raw_output_resist[interface_address], _ = tcp_interfaces[interface_address].recvfrom(2048)
-        for interface_address in tcp_interfaces:
-            for jValue in range(len(channel_map[interface_address])):
-                channel_name = channel_map[interface_address][jValue]
-                tables_list[channel_name].row['time'] = current_time
-                if float(raw_output_temp[interface_address].split(',')[jValue]) == 0.0:
-                    tables_list[channel_name].row[channel_name] = float(raw_output_resist[interface_address].split(',')[jValue])
-                else:
-                    tables_list[channel_name].row[channel_name] = float(raw_output_temp[interface_address].split(',')[jValue])
+                #Send data to I/O buffer
                 tables_list[channel_name].row.append()
-                tables_list[channel_name].flush()
 
+                #Write data to disk and clear buffer
+                tables_list[channel_name].flush()
 
         # update the plots
         plotter.update_plot(tables_list, plot_list)
@@ -209,13 +170,9 @@ try:
         # wait before reading again
         time.sleep(dt_update)
 
-        firstRun = False
-
 except KeyboardInterrupt:
     print "\nStopping data acquisition"
 
 # close the connections
 for interface_address in serial_interface_address:
     serial_interfaces[interface_address].close()
-for interface_address in tcp_interface_address:
-    tcp_interfaces[interface_address].close()
