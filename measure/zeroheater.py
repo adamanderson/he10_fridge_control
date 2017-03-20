08f6fce9fe53f61a73042d593d5c88f49f512138
Adam Anderson <adama@fnal.gov>
2017-03-19 21:38:30 -0500

    Zero everything in emergency heater zero-ing script.

--- measure/zeroheater.py
+++ measure/zeroheater.py
@@ -2,5 +2,7 @@ import he10_fridge_control.control.lakeshore as LS
 
 # setup thermometry
 ChaseLS = LS.Lakeshore350('192.168.0.12',  ['UC Head', 'IC Head', 'UC stage', 'LC shield'])
+WaferLS = LS.Lakeshore350('192.168.2.5',  ['UC Head', 'IC Head', 'UC stage', 'LC shield'])
 
 ChaseLS.set_heater_range(1, 0)
+WaferLS.set_heater_range(1, 0)
