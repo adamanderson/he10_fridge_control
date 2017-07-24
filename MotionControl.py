d2ff321beae3b81da6265ead261f60bcc368668a
as4mo3 <tosha7y@gmail.com>
2017-07-24 15:51:47 -0500

    Major function update
    Streamlined a lot of the code into functions, still need to work on the loop functions

--- MotionControl.py
+++ MotionControl.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Last version of 6/16/17
+Last version of 7/24/17
 
 @author: Antony Simonoff 
     for SPT-3G
@@ -11,18 +11,30 @@ This file prints position, velocity, and time to a text file or spreadsheet
 from __future__ import print_function
 import time
 import serial
-from datetime import datetime
+import sys
+#from datetime import datetime
 #from Tkinter import *
 #import xlsxwriter
 
+reload(sys) #sets default encoding due to ascii encoding issues
+sys.setdefaultencoding('utf8') 
+
 class NewportSMC100CC:
     def __init__(self): #initialize and open port
-        self.serial_interface = serial.Serial(port='/dev/ttyUSB0', baudrate=57600, 
+        self.serial_interface = serial.Serial(port='/dev/ttyUSB1', baudrate=57600, 
                                               bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
                                               stopbits=serial.STOPBITS_ONE, xonxoff=True)
         self.serial_interface.write('01OR\r\n')
-	time.sleep(.3) #longer wait because initialization
-	self.serial_interface.write('01VA10\r\n') #set slow velocity
+        time.sleep(.3) #longer wait because initialization
+        self.serial_interface.write('01VA10\r\n') #set slow velocity
+        
+    def set_velocity(self, velocityInput): #manually sets velocity
+        velocityWrite = "01VA" + str(velocityInput) + "\r\n"
+        self.serial_interface.write(velocityWrite)
+    
+    def set_position(self, goToDegrees):
+        positionWrite = "01PA" + str(goToDegrees) + "\r\n"
+        self.serial_interface.write(positionWrite)
         
     def get_velocity(self): #get current velocity
         self.serial_interface.write('01VA?\r\n')
@@ -35,12 +47,37 @@ class NewportSMC100CC:
         time.sleep(.1)
         position = self.serial_interface.read(self.serial_interface.inWaiting())
         return position
+
+def is_number(testStr): #test to see if a string is a number or error
+	try:
+		float(testStr)
+		return True
+	except ValueError:
+		return False
+   
+def formatter(inputStr): #formats response from device
+	inputStr= str(inputStr)
+	inputStr= inputStr.strip()
+	inputStr= inputStr.strip()[4:]
+	inputStr = float(inputStr)
+	return inputStr
+        
         
 newport = NewportSMC100CC()
 
 #interacting with user:
-numberOfLoops = input("How many loops should there be?\n\n") #Sets number of loops
-pauseBetweenLoops = input("How many seconds between loop runs? Each loop takes about 0.2s\n\n")
+
+#set velocity at beginning of file run:
+global velocityInput
+velocityInput = raw_input("What should the velocity be, in degrees per second? Leave blank for 10deg/s\n\n")
+if is_number(velocityInput) == True:
+    newport.set_velocity(velocityInput)
+if velocityInput == "":
+    veloctyInput = 10
+
+
+#set pause at beginning of file run
+pauseBetweenLoops = input("How often should data output? Each loop takes about 0.2s\n\n")
 
 
 """def show_entry_fields(): #Tkinter input/output, but I haven't figured out how to do this yet
@@ -64,22 +101,6 @@ Button(master, text='Confirm', command=show_entry_fields).grid(row=3, column=1, 
 #mainloop()
 wait_variable(numberOfLoops)"""
 
-currentLoop = 0
-outputFile = open('Output.txt', 'a')
-def is_number(testStr): #test to see if a string is a number or error
-	try:
-		float(testStr)
-		return True
-	except ValueError:
-		return False
-   
-def formatter(inputStr): #formats response from device
-	inputStr= str(inputStr)
-	inputStr= inputStr.strip()
-	inputStr= inputStr.strip()[4:]
-	inputStr = float(inputStr)
-	return inputStr
-	
 #make spreadsheet as output         
 #workbook = xlsxwriter.Workbook('Output.xlsx')
 #worksheet = workbook.add_worksheet()
@@ -92,12 +113,46 @@ def formatter(inputStr): #formats response from device
 #worksheet.write('C1', 'Time in UTC', bold)
 
 #worksheet.set_column('A:C', 15)
+
+presetDegrees = 45 #sets preset degrees around origin; + and - presetDegrees used
+outputFile = open('Output.txt', 'a')
+
+def inputFunc(): #function for all the inputs at the beginning of each run
+    goToDegrees = raw_input("Input a degree value to go to. Leave blank to choose presets: " + str(presetDegrees) + " deg, or -" + str(presetDegrees) + " deg\n\n")
+
+    if is_number(goToDegrees) == True:
+        return goToDegrees
+    else:
+        presetInput = raw_input("Enter (A) for " + str(presetDegrees) + " deg, (B) for -" + str(presetDegrees) + " deg\n\n")
+        if presetInput == "A" or presetInput == "a":
+            goToDegrees = presetDegrees
+            return goToDegrees
+        elif presetInput == "B" or presetInput == "b":  
+            goToDegrees = presetDegrees * -1
+            return goToDegrees
+        else:
+            inputFunc()
+    
+    
+
+def rotateFunc(): #function that deals with the actual rotation of the rotating stage
+    currentPosition = formatter(newport.get_position())
+    print(currentPosition)
+    currentLoop = 0
+    
+    goToDegrees = inputFunc()
+    
+    numberOfLoops = int((abs(int(currentPosition)) + abs(int(goToDegrees))/int(velocityInput))/pauseBetweenLoops)
+    print(numberOfLoops)
+    
+    newport.set_position(goToDegrees)
+    
+    for currentLoop in range(0, numberOfLoops): #loop for data output
+        currentLoop = currentLoop + 1
  
-while numberOfLoops > currentLoop: #loop for data output
-	currentLoop = currentLoop + 1
- 
-	errString_position = newport.get_position() 
-	response_position = newport.get_position() 
+        #errString_position = newport.get_position() 
+        response_position = newport.get_position() 
+        print(numberOfLoops)
 	
 	"""if is_number(response_position) == True :
 		positionPrint = stringer("Position:", response_position)
@@ -108,39 +163,39 @@ while numberOfLoops > currentLoop: #loop for data output
   
 	#worksheet.write(currentLoop, 0, formatter(response_position))
 
-	positionPrint = "Position:" + " " + str(formatter(response_position)) +"\n"
-	outputFile.write(positionPrint) 
+        positionPrint = "Position:" + " " + str(formatter(response_position)) +"\n"
+        outputFile.write(positionPrint) 
 
-	"""
-	---
-	I don't know the command to get velocity
-	VA, KV, ZT don't work
-	Need to find command then change in NewportSMC100CC.get_velocity 
-	--- 
-	response_velocity = newport.get_velocity() 
-	errString_velocity = newport.get_velocity() 
-	#if is_number(response_velocity) == True:
-	#	velocityPrint = stringer("Velocity:", response_velocity)
-	#	outputFile.write(velocityPrint)
-	#else:
-	#	velocityPrint = stringer("Velocity Error:", errString_velocity)
-	#	outputFile.write(velocityPrint)"""
- 	
-  	outputFile.write("Velocity: N/A\n")
+        velocityPrint = "Velocity:" + " " + str(velocityInput) + "\n"
+        outputFile.write(velocityPrint)
  
 	#worksheet.write(currentLoop, 1, 'N/A') #velocity placeholder
  
 	#worksheet.write(currentLoop, 2, datetime.utcnow())
 
 
-	timePrint = "Time in UTC:" + " " + str(datetime.utcnow()) +"\n"
- 	outputFile.write(timePrint)
-	timePrint = "Time in UTC:" + " " + str(time.time()) +"\n"
- 	outputFile.write(timePrint)
-	currentLoopPrint = "Current Loop:" + " " + str(currentLoop) +"\n"
-	outputFile.write(currentLoopPrint)
-	outputFile.write("\n")
-	time.sleep(pauseBetweenLoops) #sets pause length between loop runs, each run takes about 200ms
+	#timePrint = "Time in UTC:" + " " + str(datetime.utcnow()) +"\n"
+ 	#outputFile.write(timePrint)
+        timePrint = "Time in UTC:" + " " + str(time.time()) +"\n"
+        outputFile.write(timePrint)
+        currentLoopPrint = "Current Loop:" + " " + str(currentLoop) +"\n"
+        outputFile.write(currentLoopPrint)
+        outputFile.write("\n")
+        time.sleep(pauseBetweenLoops) #sets pause length between loop runs, each run takes about 200ms
+
+def continueFunc():
+    continueInput = raw_input("Continue Y/N? ")
+    if continueInput == "Y" or continueInput == "y":
+        continueInput = "Yes"
+        rotateFunc()
+    elif continueInput == "N" or continueInput == "n":
+        continueInput = "No"
+        outputFile.close()
+        sys.exit()
+    else:
+        continueFunc()
+
+continueFunc()
 
 outputFile.close()
 #workbook.close()
