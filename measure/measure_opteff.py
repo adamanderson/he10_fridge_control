a6c44bfa9d2aafdb41a7c917ceacad3940e864eb
Adam Anderson <adama@fnal.gov>
2018-08-23 23:48:18 -0500

    Miscellaneous changes to optical efficiency measurement.

--- measure/measure_opteff.py
+++ measure/measure_opteff.py
@@ -19,21 +19,21 @@ import cPickle as pickle
 overbias_amp = 0.018
 dropbolos_step = 0.000025
 dropbolos_tolerance = 0.02
-coldload_temp_setpoints = [10, 12, 14, 16, 18, 20, 22, 24]
+coldload_temp_setpoints = [7, 10, 12, 14, 16, 18, 20, 22, 24]
 
 # cryostat-specific settings
-logfile = '/daq/fnal_temp_logs/run17_log_read.h5'
+logfile = '/daq/fnal_temp_logs/run31_log_read.h5'
 blackbody_channame = 'blackbody'
 coldload_channame = 'cold load center'
 stage_channame = 'UC stage'
-WaferLS = LS.Lakeshore350('192.168.2.5',
-                          ['UC stage', 'channel B', 'channel C', 'channel D'])
-WaferLS.set_heater_range(1, 3)
-p = pid.PIDController('/daq/fnal_temp_logs/run17_log_read.h5', 'cold load center',
-                      WaferLS, 1, 0.8, 0.01, 0.0, coldload_temp_setpoints[0])
+WaferLS = LS.Lakeshore350('192.168.0.12',
+                          ['UC Head', 'IC Head', 'UC stage', 'LC shield'])
+WaferLS.set_heater_range(2, 4)
+p = pid.PIDController('/daq/fnal_temp_logs/run31_log_read.h5', 'cold load center',
+                      WaferLS, 2, 0.8, 0.015, 0.0, coldload_temp_setpoints[0])
 
 # setup pydfmux stuff
-hwm_file = '/home/adama/hardware_maps/fnal/run17/hwm_SPTpol_only/hwm.yaml'
+hwm_file = '/home/adama/hardware_maps/fnal/run31/hwm.yaml'
 y = pydfmux.load_session(open(hwm_file, 'r'))
 hwm = y['hardware_map']
 bolos = hwm.query(pydfmux.Bolometer)
@@ -57,11 +57,8 @@ for jtemp in range(len(coldload_temp_setpoints)):
     print('Setting coldload to {} K.'.format(coldload_temp_setpoints[jtemp]))
     p.set_temp_setpoint(coldload_temp_setpoints[jtemp])
     
-    # wait 1h for blackbody to stabilize
-    if jtemp==0:
-        time.sleep(1200)
-    else:
-        time.sleep(2400)
+    # wait 40m for blackbody to stabilize
+    time.sleep(1800)
 
     # once blackbody has stabilized, take an IV curve then overbias
     housekeeping['{} start temp'.format(stage_channame)].append(gt.gettemp(logfile, stage_channame))
