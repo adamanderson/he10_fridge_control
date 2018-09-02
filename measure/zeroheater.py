a328e902a3446fb8cc036ff7317776b6d256cc51
Adam Anderson <adama@fnal.gov>
2018-09-02 16:27:21 -0500

    Zero heater script should actually zero all the heaters.

--- measure/zeroheater.py
+++ measure/zeroheater.py
@@ -2,7 +2,6 @@ import he10_fridge_control.control.lakeshore as LS
 
 # setup thermometry
 ChaseLS = LS.Lakeshore350('192.168.0.12',  ['UC Head', 'IC Head', 'UC stage', 'LC shield'])
-WaferLS = LS.Lakeshore350('192.168.2.5',  ['UC Head', 'IC Head', 'UC stage', 'LC shield'])
 
-ChaseLS.set_heater_range(1, 0)
-WaferLS.set_heater_range(1, 0)
+for jheater in range(1,5):
+    ChaseLS.set_heater_range(jheater, 0)
